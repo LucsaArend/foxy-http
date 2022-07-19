b949124f26e7d33ad27acb34b3f32087886397ca
Lucas Arend <lucasarend@night-fox.net>
2022-07-19 11:12:45 -0300

    Post Support Array
    Add suporte in function sendPost for array

--- src/HttpFox.php
+++ src/HttpFox.php
@@ -45,6 +45,9 @@ class HttpFox
     public function sendPost($prURL,$prData){
         curl_setopt($this->ch, CURLOPT_URL,$prURL);
         curl_setopt($this->ch, CURLOPT_POST, 1);
+        if (is_array($prData)) {
+            $prData = http_build_query($prData);
+        }
         curl_setopt($this->ch, CURLOPT_POSTFIELDS,$prData);
 
         $this->responseText = curl_exec($this->ch);
